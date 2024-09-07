d50b80f325673723e52dfe3519d94f5a03b3174e
jmsmg <jmsmg1@me.com>
2024-09-07 16:36:36 +0900

    add AcountToken model, Room model

--- django/src/game/models.py
+++ django/src/game/models.py
@@ -1,9 +1,13 @@
+from user.models import User
 from django.db import models
 
 class Room(models.Model):
-	room_name = models.TextField()
+	room_name = models.CharField(max_length=255)
 	goal_point = models.IntegerField(default=10)
-	password = models.TextField()
-	player1 = models.TextField()
-	player2 = models.TextField()
-	status = models.TextField()
+	room_password = models.CharField()
+	player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player1_rooms')
+	player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player2_rooms')
+	status = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.room_name
