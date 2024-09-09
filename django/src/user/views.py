d4446055cf858ca2586c4eb5f1d019a76e4ea4a4
jmsmg <jmsmg1@me.com>
2024-09-09 15:13:06 +0900

    로그아웃 부분 재삽입

--- django/src/user/views.py
+++ django/src/user/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from .serializers import UserSignupSerializer, UserLoginSerializer
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 class UserRegistrationView(generics.CreateAPIView):
@@ -21,6 +23,22 @@ class UserRegistrationView(generics.CreateAPIView):
             first_error_key = next(iter(serializer.errors))
             first_error_message = serializer.errors[first_error_key][0]
             return Response({first_error_key: first_error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get('refresh')
+
+        if refresh_token:
+            try:
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+                return Response({"detail": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+            except Exception as e:
+                return Response({"detail": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"detail": "Refresh token required"}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(TokenObtainPairView):
     serializer_class = UserLoginSerializer
