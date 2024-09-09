0267fe26f48fd03673ef8f66d532f8df94e33393
jmsmg <jmsmg1@me.com>
2024-09-09 13:42:07 +0900

    create 함수 오버라이딩

--- django/src/user/views.py
+++ django/src/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from .serializers import UserSignupSerializer, UserLoginSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -11,6 +11,17 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserSignupSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({"result": "ok"}, status=status.HTTP_201_CREATED)
+        else:
+            # 첫번째 에러만 반환하는 로직
+            first_error_key = next(iter(serializer.errors))
+            first_error_message = serializer.errors[first_error_key][0]
+            return Response({first_error_key: first_error_message}, status=status.HTTP_400_BAD_REQUEST)
+
 class UserLoginView(TokenObtainPairView):
     serializer_class = UserLoginSerializer
 
@@ -19,4 +30,3 @@ class UserLoginView(TokenObtainPairView):
 @permission_classes([IsAuthenticated])
 def LoginCheckView(request):
     return Response({'message': 'This is a protected view.'})
-    
