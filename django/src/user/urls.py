926da50f2fd039caa35e30e071a1bf6d4e2bdbe7
leebo155 <bohlee@student.42seoul.kr>
2024-09-10 11:36:43 +0900

    fix: 토큰 쿠키에 저장

--- django/src/user/urls.py
+++ django/src/user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('refresh/', views.UserRefreshView.as_view(), name='refresh'),
     path('signup/', views.UserRegistrationView.as_view(), name='signup'),
     path('check/', views.LoginCheckView, name='login_check'),
 ]
--- django/src/user/views.py
+++ django/src/user/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
@@ -34,17 +34,42 @@ class UserLogoutView(APIView):
             try:
                 token = RefreshToken(refresh_token)
                 token.blacklist()
-                return Response({"detail": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+                response = Response({"detail": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT)
+                response.delete_cookie('access')
+                response.delete_cookie('refresh')
+                return response
             except Exception as e:
                 return Response({"detail": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"detail": "Refresh token required"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"refresh": "This field is required"}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(TokenObtainPairView):
     serializer_class = UserLoginSerializer
 
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh', None) and response.data.get('access', None):
+            cookie_max_age = 60 * 60 * 24
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True, secure=True)
+            cookie_max_age = 60 * 30
+            response.set_cookie('access', response.data['access'], max_age=cookie_max_age, httponly=True, secure=True)
+            del response.data['access']
+            del response.data['refresh']
+            response.data = {"detail": "Login successful"}
+        return super().finalize_response(request, response, *args, **kwargs)
+
+class UserRefreshView(TokenRefreshView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh', None) and response.data.get('access', None):
+            cookie_max_age = 60 * 60 * 24
+            response.set_cookie('refresh', response.data['refresh'], max_age=cookie_max_age, httponly=True, secure=True)
+            cookie_max_age = 60 * 30
+            response.set_cookie('access', response.data['access'], max_age=cookie_max_age, httponly=True, secure=True)
+            del response.data['access']
+            del response.data['refresh']
+            response.data = {"detail": "Refresh successful"}
+        return super().finalize_response(request, response, *args, **kwargs)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def LoginCheckView(request):
-    return Response({'message': 'This is a protected view.'})
+    return Response({'detail': 'This is a protected view.'})
