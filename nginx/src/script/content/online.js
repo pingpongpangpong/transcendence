c745478ea9563427d12e771177cbc6cb9995ab41
juhyeonlee <griiim134@gmail.com>
2024-09-23 11:54:02 +0900

    fix: show room list

--- nginx/src/script/content/online.js
+++ nginx/src/script/content/online.js
@@ -1,9 +1,57 @@
+import { lang, langIndex } from "../lang.js";
 import { checkUser } from "../tab.js";
 import { online } from "./feature.js";
 import { getGamePoint } from "./feature.js";
 
 const onlineContainer = document.getElementById("online");
 const roomSettingContainer = document.getElementById("room-setting");
+const roomListContainer = document.getElementById("room-list");
+
+// room list
+function showRoomList() {
+	fetch("").then((response) => {
+		if (response.status === 200) {
+			return response.json();
+		} else {
+			alert("방을 불러오는데 실패 했습니다.");
+		}
+	}).then((json) => {
+		const roomList = json.roomlist;
+		roomList.foreach((room) => {
+			makeRoom(room);
+		});
+	});
+}
+
+function makeRoom(room) {
+	const container = document.createElement("div");
+	container.id = `room-${room.roomname}`;
+	container.className = "window room";
+
+	const info = document.createElement("div");
+	info.id = `room-${room.roomname}-info`;
+	info.className = "window room-info";
+
+	const name = document.createElement("h4");
+	name.textContent = `${lang[langIndex].roomName}: ${room.roomname}`;
+	
+	const player = document.createElement("h6");
+	player.textContent = `${lang[langIndex].roomHost}: ${room.player1}`;
+
+	info.appendChild(name);
+	info.appendChild(player);
+
+	const btn = document.createElement("button");
+	btn.id = `room-${room.roomname}-btn`;
+	btn.className = "room-btn";
+	btn.textContent = lang[langIndex].enter;
+
+	container.appendChild(info);
+	container.appendChild(btn);
+	roomListContainer.appendChild(container);
+}
+
+showRoomList();
 
 // open room setting
 document.getElementById("make-room-btn").addEventListener("click", () => {
