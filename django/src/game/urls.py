23ddcb38d703dc0ee61a39e071b136497cfcea12
jmsmg <jmsmg1@me.com>
2024-09-23 13:49:17 +0900

    add slash

--- django/src/game/urls.py
+++ django/src/game/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create-room", views.createRoom, name='create-room'),
-    path("list-room", views.listRoom, name='list-room'),
+    path("create-room/", views.createRoom, name='create-room'),
+    path("list-room/", views.listRoom, name='list-room'),
     # path("join-room", views.joinRoom, name='join-room'),
     # path("search-room", views.searchRoom, name='search-room'),
     # path("finish-room", views.finishRoom, name='finish-room'),
